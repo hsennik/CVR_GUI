7407be50b555fd3934d518d8ce2aa88261380278
Hannah Sennik <hsennik@uwaterloo.ca>
2016-06-30 16:09:17 -0400

    changed position of viewports for plots

--- viewboxcar.m
+++ viewboxcar.m
@@ -2,15 +2,17 @@ function viewboxcar(source,callbackdata,subj,dir_input,HV)
 
 if HV ==1
     subj.breathhold = 'HV';
+else
+    subj.breathhold = 'BH1';
 end
     stimlocation = strcat(dir_input,'/metadata/stim/bhonset',subj.name,'_',subj.breathhold,'_customized.1D');
     newplot = load(stimlocation);
     if HV ==1
         name = 'Customized HV boxcar plot';
-        pos = [950,400,500,300];
+        pos = [950,300,500,300];
     else
         name = 'Customized BH boxcar plot';
-        pos = [400,400,500,300];
+        pos = [400,300,500,300];
     end
     
     figure('Name',name,...
