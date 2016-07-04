b72e0cc829b5ebe2a04cec8f45e87be67e6cd8bd
Hannah Sennik <hsennik@uwaterloo.ca>
2016-07-04 15:12:09 -0400

    matlab files that aren't used for final CVR GUI

--- menu_call.m
+++
@@ -1,6 +0,0 @@
-function menu_call(source,callbackdata,mp)
-
-selection = get(source,'Value');
-display(selection);
-
-end
--- select_nii.m
+++
@@ -1,6 +0,0 @@
-function loadimage(source,callbackdata,image,path,image_var)
-
-[image,path] = uigetfile('*.nii','/data/projects/CVR/metadata/sandbox');
-image_var = load_nii([path '/' image]);
-
-end
