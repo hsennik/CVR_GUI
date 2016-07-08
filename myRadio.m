cf0274504c4bc81a71104992993f551b2015e7e7
Hannah Sennik <hsennik@uwaterloo.ca>
2016-07-08 09:37:45 -0400

    setting handles and writing to standard_or_boxcar file

--- myRadio.m
+++ myRadio.m
@@ -15,32 +15,35 @@ handles = guidata(source);
 otherRadio = handles.boxcar(handles.boxcar ~= source);
 set(otherRadio,'Value',0);
 
-fileID = fopen(strcat(dir_input,'customize_boxcar.txt'),'w+'); % open customized boxcar textfile in write mode 
+fileID = fopen(strcat(dir_input,'textfiles/standard_or_custom.txt'),'w+'); % open customized boxcar textfile in write mode 
 format = '%d\n';
 
 if handles.boxcar(1).Value == 1
-    fprintf(fileID,format,0); % If the standard boxcar was selected, write a 0 to the customized boxcar textfile 
-    fclose(fileID);
     display('Use standard stim file');
-    mkdir(dir_input,'flirt/standard_boxcar'); %  Make standard directories in the flirt folder (for transformations and pf stimfiles)
-    mkdir(dir_input,'flirt/standard_boxcar_not_processed');
+    fprintf(fileID,format,0); % If the standard boxcar was selected, write a 0 to the file
     figures_to_close = findall(0,'Type','figure'); %  Close all figures except for the main one ('Process and Analyze Subject')
     close(figures_to_close(2:end));
     set(handles.start,'Enable','on'); %  Allow user to start processing and analyzing subject
     set(handles.custom(1),'Value',0); %  Set the customized checkboxes to unchecked
     set(handles.custom(2),'Value',0);
+    set(handles.custom(3),'Value',0);
+    set(handles.custom(4),'Value',0);
     set(handles.custom(1),'Enable','off'); %  Disable the customized checkboxes 
     set(handles.custom(2),'Enable','off');
+    set(handles.custom(3),'Enable','off');
+    set(handles.custom(4),'Enable','off');
+else
+    set(handles.start,'Enable','off');
 end
 
 if handles.boxcar(2).Value == 1
-    fprintf(fileID,format,1); % If customized boxcar was selected, write a 1 to the customized boxcar file 
-    fclose(fileID);
     display('Use customized stim file');
-    mkdir(dir_input,'flirt/customized_boxcar'); %  Make customized directories in the flirt folder 
-    mkdir(dir_input,'flirt/customized_boxcar_not_processed');
+    fprintf(fileID,format,1); % If the customized boxcar was selected, write a 1 to the file 
     set(handles.custom(1),'Enable','on'); %  Enable the customized checkboxes to specify customization for BH1, BH2, and/or HV
     set(handles.custom(2),'Enable','on');
+    set(handles.custom(3),'Enable','on');
+    set(handles.custom(4),'Enable','on');
     set(handles.start,'Enable','off'); %  Do not allow the user to press the start button yet 
 end
+fclose(fileID);
 end
