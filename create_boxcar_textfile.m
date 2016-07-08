47bd40a95b3cbc6574ad4a6b853ffbe39dfc8322
Hannah Sennik <hsennik@uwaterloo.ca>
2016-07-08 09:31:34 -0400

    added breath hold selection and error checking

--- create_boxcar_textfile.m
+++ create_boxcar_textfile.m
@@ -1,64 +1,110 @@
-function create_boxcar_textfile(source,callbackdata,subj,dir_input,HV)
+function create_boxcar_textfile(source,callbackdata,subj,dir_input,breath,sp,same_boxcar)
+% Function that creates the boxcar stimfile from user entered data
+% 
+% INPUTS 
+%     subj - subject data (name, breathhold, date)
+%     dir_input - directory where data should we stored
+%     breath - determines which breathhold the boxcar is for 
+%     sp - main GUI data 
+% 
+% *************** REVISION INFO ***************
+% Original Creation Date - June 22, 2016
+% Author - Hannah Sennik
 
+%  Get data from the 'Create customized BH boxcar' figure
 handles = guidata(source);
-if HV == 1
-    subj.breathhold = 'HV';
-else
+
+%  Set subj.breathhold based on variable HV from create_boxcar.m or
+%  create_HVboxcar.m
+
+if breath == 1
     subj.breathhold = 'BH1';
+elseif breath == 2
+    subj.breathhold = 'BH2';
+elseif breath == 3
+    subj.breathhold = 'HV';
 end
-%  Create while loop that allows user to keep doing this until they press
-%  USE BOXCAR BUTTON 
+
 customized_stimfile = strcat(dir_input,'metadata/stim/bhonset',subj.name,'_',subj.breathhold,'_customized.1D'); % Create the textfile for the stimfile 
 fileID = fopen(customized_stimfile,'w+');
 format = '%d\n';
-display('customized stim file created');
+display('Customized stim file generated');
 
-number_blocks= str2double(handles.number_blocks.String); % Convert all the data entry strings in to doubles
-display(number_blocks);
+% Convert all the data entry strings in to doubles and divide time blocks
+% by two to match with TR 
+number_blocks= str2double(handles.number_blocks.String); 
+
 start_delay = str2double(handles.start_delay.String);
-start_delay = floor(start_delay/2.0);
-display(start_delay);
+halved_start_delay = floor(start_delay/2.0);
+
 break_duration = str2double(handles.break_duration.String);
-break_duration = floor(break_duration/2.0);
-display(break_duration);
+halved_break_duration = floor(break_duration/2.0);
+
 break_after_block = str2double(handles.break_after_block.String);
-display(break_after_block);
+
 breathhold_duration = str2double(handles.breathhold_duration.String);
-breathhold_duration = floor(breathhold_duration/2.0);
-display(breathhold_duration);
+halved_breathhold_duration = floor(breathhold_duration/2.0);
+
 normal_breathing_duration = str2double(handles.normal_breathing_duration.String);
-normal_breathing_duration = floor(normal_breathing_duration/2.0);
-display(normal_breathing_duration);
-         
+halved_normal_breathing_duration = floor(normal_breathing_duration/2.0);
+
+%  Check the value of variables          
+display(number_blocks);
+display(halved_start_delay);
+display(halved_break_duration);
+display(break_after_block);
+display(halved_breathhold_duration);
+display(halved_normal_breathing_duration);
+
 % Print 0 to file where the subject is breathing normally, and 185
 % where the subject is holding their breath 
-if start_delay ~= 0
-    for h = 1:start_delay  
+if halved_start_delay ~= 0
+    for h = 1:halved_start_delay  
         fprintf(fileID,format,0);
     end
 end
 
-for i = 1:number_blocks
+for i = 1:number_blocks %  Create the specified number of blocks 
     if i == break_after_block
-        normal_breathing_duration = normal_breathing_duration + break_duration;
+        halved_normal_breathing_duration = halved_normal_breathing_duration + halved_break_duration; %  add the break duration to break_after_block
     end
     if i == break_after_block + 1
-        normal_breathing_duration = normal_breathing_duration - break_duration;
+        halved_normal_breathing_duration = halved_normal_breathing_duration - halved_break_duration; %  remove the break duration for all block after break_after_block
     end
-    for j = 1:breathhold_duration
+    for j = 1:halved_breathhold_duration
         fprintf(fileID,format,185);
     end
-    for k = 1:normal_breathing_duration
+    for k = 1:halved_normal_breathing_duration
         fprintf(fileID,format,0);
     end
 end
 
 fclose(fileID);
-if strcmp(subj.breathhold,'BH1') == 1
+
+if breath == 1 || breath == 2
+    % Check that all of the times add up to 370 seconds
+    % Check that blocks add up to 60 seconds 
+    total_block_time = normal_breathing_duration + breathhold_duration;
+    if total_block_time ~= 60
+        errormessage = errordlg('Normal breathing duration and breathhold duration should add up to 60 seconds');
+        set(handles.viewboxcar,'Enable','off');
+    else
+        set(handles.viewboxcar,'Enable','on'); % Enable viewboxcar pushbutton
+        set(sp.start,'Enable','on');
+    end
+end 
+
+if breath == 3
+    set(handles.viewboxcar,'Enable','on'); % Enable viewboxcar pushbutton
+    set(sp.start,'Enable','on');
+end
+        
+
+if same_boxcar == 1 %  if user selected for BH1 and BH2 boxcars to be the same 
     subj.breathhold = 'BH2';
     copyfile(customized_stimfile,strcat(dir_input,'metadata/stim/bhonset',subj.name,'_',subj.breathhold,'_customized.1D'),'f');
-    set(handles.HVboxcar,'Enable','on');
+%     set(handles.HVboxcar,'Enable','on');
+    display('boxcar copied for BH2');
 end
-set(handles.viewboxcar,'Enable','on'); % Enable viewboxcar pushbutton
 
 end
