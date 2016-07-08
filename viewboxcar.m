7eb340f7d3ed58c58e378c6a64ee901c7f98ff22
Hannah Sennik <hsennik@uwaterloo.ca>
2016-07-08 09:36:26 -0400

    added breathhold dependency

--- viewboxcar.m
+++ viewboxcar.m
@@ -1,23 +1,25 @@
-function viewboxcar(source,callbackdata,subj,dir_input,HV)
+function viewboxcar(source,callbackdata,subj,dir_input,breath)
 
-if HV ==1
-    subj.breathhold = 'HV';
-else
+if breath == 1
     subj.breathhold = 'BH1';
+    name = 'Customized BH1 boxcar plot';
+    pos = [400,300,500,300];
+elseif breath == 2
+    subj.breathhold = 'BH2';
+    name = 'Customized BH2 boxcar plot';
+    pos = [950,300,500,300];
+elseif breath == 3
+    subj.breathhold = 'HV';
+    name = 'Customized HV boxcar plot';
+    pos = [1500,300,500,300];
 end
-    stimlocation = strcat(dir_input,'/metadata/stim/bhonset',subj.name,'_',subj.breathhold,'_customized.1D');
-    newplot = load(stimlocation);
-    if HV ==1
-        name = 'Customized HV boxcar plot';
-        pos = [950,300,500,300];
-    else
-        name = 'Customized BH boxcar plot';
-        pos = [400,300,500,300];
-    end
+
+stimlocation = strcat(dir_input,'/metadata/stim/bhonset',subj.name,'_',subj.breathhold,'_customized.1D');
+newplot = load(stimlocation);
     
-    figure('Name',name,...
-           'Visible','on',...
-            'Position',pos); 
-    plot(newplot); 
+figure('Name',name,...
+       'Visible','on',...
+        'Position',pos); 
+plot(newplot); 
     
 end
