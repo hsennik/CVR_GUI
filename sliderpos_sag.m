7b84e535b20dc09aaf6778d12b7a4a4e81cab824
Hannah Sennik <hsennik@uwaterloo.ca>
2016-07-12 14:57:25 -0400

    CVR maps are made only in CVRmap.m

--- sliderpos_sag.m
+++ sliderpos_sag.m
@@ -1,4 +1,4 @@
-function sliderpos_sag(source,callbackdata,anat,mp,funct,sag_window)
+function sliderpos_sag(source,callbackdata,anat,mp,funct,sag_window,dir_input,subj)
 % Function to update the sagittal slice displayed based on the sagittal slider position 
 % 
 % INPUTS 
@@ -11,21 +11,10 @@ function sliderpos_sag(source,callbackdata,anat,mp,funct,sag_window)
 % Original Creation Date - May 26, 2016
 % Author - Hannah Sennik
 
+global sag_slider_value;
 sag_slider_value = get(source, 'Value'); % get the value of the slider position
-
 display(sag_slider_value);
-
 set(sag_window.position_slider,'String',floor(sag_slider_value)); % Display the updated slider value in the sagittal slice window 
+slider_position('saggital',anat,mp,funct,sag_window,dir_input,subj);
 
-anat.slice_x = anat.slice_x + floor(sag_slider_value - anat.slice_x); % Increase the slice position based on the slider value 
-updated_slice = (double(repmat(imresize(squeeze(anat.img(anat.slice_x,:,:)),[anat.y anat.z]),[1 1 3]))- anat.sigmin) / anat.sigmax;
-updated_slice = imresize(updated_slice,[anat.y anat.z/anat.hdr.dime.pixdim(3)]);
-updated_slice = rot90(updated_slice(anat.yrange,anat.zrange,:)); % Rotate and flip the slice so that it is displayed correctly to the user 
-updated_slice = flip(updated_slice,2);
-
-if (mp.CVRb.Value == 1) % If the CVR button is pressed, call the function to overlay CVR map 
-   CVRmap_function_saggital(anat,funct,mp); 
-else % Else, just display the anatomical slice
-   imshow(updated_slice);
-end
 end
