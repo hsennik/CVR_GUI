f421e058cc944db0a0064ac935d501eb7bc0118a
Hannah Sennik <hsennik@uwaterloo.ca>
2016-07-12 14:54:04 -0400

    added comments, changed strcat

--- drawROI.m
+++ drawROI.m
@@ -16,22 +16,23 @@ global ax_slider_value;
 addpath('/data/wayne/matlab/NIFTI'); % add path to nii functions
 addpath('/data/wayne/matlab/general');
 
-cd(dir_input);
+cd(dir_input); % change in to subject's directory 
 
 mkdir(dir_input,'/timeseries'); % make a directory to save ROI mask and timeseries text file 
 
 data_out = anat; % creating a struct with same header info as anat (this will be used for the mask)
 
-fileID = fopen(strcat(dir_input,'/textfiles/processing.txt'),'r'); % open processing text file to see if user selected processed or raw data 
+fileID = fopen([dir_input '/textfiles/processing.txt'],'r'); % open processing text file to see if user selected processed or raw data 
 format = '%d';
 A = fscanf(fileID,format);
+
 if A == 1
     tag = 'processed';
 else
     tag = 'processed_not';
 end
 
-fname = strcat('data/',tag,'/CVR_',subj.date,'/final/',subj.name,'_',subj.breathhold,'_CVR_',subj.date,'.nii');
+fname = ['data/' tag '/CVR_' subj.date '/final/' subj.name '_' subj.breathhold '_CVR_' subj.date '.nii'];
 processed = load_nii([dir_input '/' fname]); % load "processed" functional data 
 
 [processed.x,processed.y,processed.z] = size(processed.img);
@@ -48,51 +49,58 @@ new(:,:,z_index) = binaryImage; % fill correct indices of new img with mask
 display('new img created');
 
 data_out.img = new; % use new as the img for the data_out struct
-save_mask = strcat('timeseries/mask.nii'); % save the mask as a nii to timeseries directory
+save_mask = 'timeseries/mask.nii'; % save the mask as a nii to timeseries directory
 save_nii(data_out,save_mask);
 
-display('YES nii saved');
+display('nii mask saved');
 
-fileID = fopen(strcat(dir_input,'/textfiles/standard_or_custom.txt'),'r'); % open the customize boxcar file
+fileID = fopen([dir_input '/textfiles/standard_or_custom.txt'],'r'); % open the customize boxcar file
 format = '%d';
-A = fscanf(fileID,format);
+B = fscanf(fileID,format);
 
 % Determine which stimfile was used so that it can be displayed with the
 % timeseries 
-if(mp.menu(1).Value == 2)
-    if A == 1 && strcmp(mp.boxcarsel.String,'Boxcar selection: customized') == 1
-        stim = strcat('metadata/stim/bhonset',subj.name,'_',subj.breathhold,'_customized.1D');
+if(mp.menu(1).Value == 2) % boxcar was selected on the main GUI 
+    if B == 1 && strcmp(mp.boxcarsel.String,'Boxcar selection: customized') == 1
+        stim = ['metadata/stim/bhonset' subj.name '_' subj.breathhold '_customized.1D'];
     else
-        stim = strcat('metadata/stim/bhonset',subj.name,'_',subj.breathhold,'.1D');
+        stim = ['metadata/stim/bhonset' subj.name '_' subj.breathhold '.1D'];
     end
-elseif(mp.menu(1).Value == 3)
+elseif(mp.menu(1).Value == 3) % pf was selected on the main GUI 
     if strcmp(tag,'processed_not') == 1
-        stim = strcat('metadata/stim/pf_',subj.breathhold,'_stim_not_processed.1D');
+        stim = ['metadata/stim/pf_' subj.breathhold '_stim_not_processed.1D'];
     else
-        stim = strcat('metadata/stim/pf_',subj.breathhold,'_stim.1D');
+        stim = ['metadata/stim/pf_' subj.breathhold '_stim.1D'];
     end
 end
 
 % Transform the mask from anatomical space to functional space - save as
 % finalmask.nii 
-command = strcat('flirt -in timeseries/mask.nii -ref data/',tag,'/CVR_',subj.date,'/final/',subj.name,'_',subj.breathhold,'_CVR_',subj.date,'.nii -out timeseries/finalmask.nii -init timeseries/anat2funct.mat -applyxfm');
+command = ['flirt -in timeseries/mask.nii -ref data/' tag '/CVR_' subj.date '/final/' subj.name '_' subj.breathhold '_CVR_' subj.date '.nii -out timeseries/finalmask.nii -init timeseries/anat2funct.mat -applyxfm'];
 status= system(command);
+
 % Use 3dmaskave to mask the functional data with finalmask.nii and save the
 % timeseries to timeseries.1D in timeseries directory 
-command = strcat('3dmaskave -q -mask timeseries/finalmask.nii data/',tag,'/CVR_',subj.date,'/final/',subj.name,'_',subj.breathhold,'_CVR_',subj.date,'.nii > timeseries/timeseries.1D');
+command = ['3dmaskave -q -mask timeseries/finalmask.nii data/' tag '/CVR_' subj.date '/final/' subj.name '_' subj.breathhold '_CVR_' subj.date '.nii > timeseries/timeseries.1D'];
 status = system(command);
 
 %  Load in the 1D timeseries file and display as plot 
-timeseries = strcat(dir_input,'/timeseries/timeseries.1D');
+timeseries = [dir_input '/timeseries/timeseries.1D'];
 timeseries_plot = load(timeseries);
+
 ts = figure('Name','Timeseries',...
        'Visible','on',...
-       'Numbertitle','off');  
-plot(timeseries_plot);
+       'Numbertitle','off',...
+       'Position', [1130,400,600,410]);
+set(mp.f, 'MenuBar', 'none'); % remove the menu bar 
+set(mp.f, 'ToolBar', 'none'); % remove the tool bar   
+
+plot(timeseries_plot);  % plot the timeseries from the ROI 
 title('Timeseries vs. Stimulus')
 xlabel('Time')
 ylabel('BOLD Signal')
 hold; % hold the plot 
+
 stimfile = load(stim); % load the stimfile used to generate the parametric map 
 stimfile = stimfile + (median(timeseries_plot) - median(stimfile)) + 50; % move the plot up so that user can easily compare timeseries and stim
 plot(stimfile); % plot the stimfile 
