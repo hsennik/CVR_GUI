bffecef3c1a2b313319fa243774f8a716b151d75
Hannah Sennik <hsennik@uwaterloo.ca>
2016-08-09 15:12:00 -0400

    commented checking if analysis is already done

--- standard_selected.m
+++ standard_selected.m
@@ -15,10 +15,10 @@ handles = guidata(source);
 close(figures_to_close(2:end)); % close all figures except the main interface with all the controls 
 set(handles.start,'Enable','on'); % enable the analyze button 
  
-if exist(['data/analyzed_standard_boxcar/CVR_' subj.date '/final/' subj.name '_' subj.breathhold '_CVR_' subj.date '_glm_buck.nii'],'file') == 2 % if the standard boxcar analysis was done for this breathhold then dont do it again 
-    set(handles.start,'Enable','off');
-    set(handles.look,'Enable','on');
-end       
+% if exist(['data/analyzed_standard_boxcar/CVR_' subj.date '/final/' subj.name '_' subj.breathhold '_CVR_' subj.date '_glm_buck.nii'],'file') == 2 % if the standard boxcar analysis was done for this breathhold then dont do it again 
+%     set(handles.start,'Enable','off');
+%     set(handles.look,'Enable','on');
+% end       
 
 pos = [358,800,900,670]; % initialize the position of the window to show the average brain timeseries plot
 stim = [directories.subject '/' directories.metadata '/stim/bhonset' subj.name '_' subj.breathhold '.1D']; % initialize the stimfile selection as the standard boxcar
